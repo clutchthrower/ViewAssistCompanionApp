724657ca122e0034e47923163f8818d491ef1b9f
Mark Parker <msparker@sky.com>
2026-05-22 18:37:06 +0100

    v0.11.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.msp1974.vacompanion"
         minSdk = 26
         targetSdk = 36
-        versionName = "0.11.2"
+        versionName = "0.11.3"
         versionCode = code
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
